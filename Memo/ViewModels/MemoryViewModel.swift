9c04204ce7539bfce14180b4f240f227a6cd6125
co5ta <36671610+co5ta@users.noreply.github.com>
2021-08-15 23:22:28 +0200

    Blank out text (#3)
    Blank out text and show words progressively on tap

--- Memo/ViewModels/MemoryViewModel.swift
+++ Memo/ViewModels/MemoryViewModel.swift
@@ -6,7 +6,36 @@
 //
 
 import Foundation
+import Combine
 
-struct MemoryViewModel {
+class MemoryViewModel {
     var item: Item
+    @Published var blankCounter = 0
+    
+    init(item: Item) {
+        self.item = item
+    }
+    
+    func getWords() -> [(value: String, isVisible: Bool)] {
+        let words = item.text.components(separatedBy: " ")
+        var output: [(value: String, isVisible: Bool)] = []
+        
+        for (index, word) in words.enumerated() {
+            if index < blankCounter {
+                output.append((value: word, isVisible: true))
+            } else {
+                var strippedWord = word
+                var punctuation: String?
+                if let char = word.last, ".,".contains(char) {
+                    punctuation = String(strippedWord.removeLast())
+                }
+                output.append((value: strippedWord, isVisible: false))
+                if let punctuation = punctuation {
+                    output.append((value: punctuation, isVisible: true))
+                }
+            }
+            output.append((value: " ", isVisible: true))
+        }
+        return output
+    }
 }
--- MemoTests/MemoTests.swift
+++ MemoTests/MemoTests.swift
@@ -23,4 +23,25 @@ class MemoTests: XCTestCase {
         let viewModel = MainViewModel()
         XCTAssertEqual(viewModel.items.count, 8) 
     }
+    
+    func testItemTextBlanking() {
+        let items = Storage.shared.loadItems()
+        let viewModel = MemoryViewModel(item: items[0])
+        let words = viewModel.getWords().filter { !" .,".contains($0.value) && $0.isVisible }
+        let visibleWords = words.filter { $0.isVisible }
+        let invisibleWords = words.filter { !$0.isVisible }
+        XCTAssertEqual(visibleWords.count, 0)
+        XCTAssertEqual(invisibleWords.count, words.count)
+    }
+    
+    func testItemTextBlankingWithBlankCounterNotZero() {
+        let items = Storage.shared.loadItems()
+        let viewModel = MemoryViewModel(item: items[0])
+        viewModel.blankCounter = 5
+        let words = viewModel.getWords().filter { !" .,".contains($0.value) && $0.isVisible }
+        let visibleWords = words.filter { $0.isVisible }
+        let invisibleWords = words.filter { !$0.isVisible }
+        XCTAssertEqual(visibleWords.count, viewModel.blankCounter)
+        XCTAssertEqual(invisibleWords.count, words.count - viewModel.blankCounter)
+    }
 }
